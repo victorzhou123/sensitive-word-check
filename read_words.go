a4833c953f3d938a0a86385938f24639b7fece8f
VictorZhou123 <victorzhoux@163.com>
2024-10-09 00:08:52 +0800

    feat: read sensitive words files

---
+++ read_words.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+const (
+	filePathAd             = "./sensitive-word/ad.txt"
+	filePathGunsExplosives = "./sensitive-word/guns_explosives.txt"
+	filePathPolitic        = "./sensitive-word/politic.txt"
+	filePathWebsite        = "./sensitive-word/website.txt"
+)
+
+type sensitiveWords struct {
+	filePaths []string
+	skipWords map[string]struct{}
+
+	words []string
+}
+
+func newSensitiveWords(skipWords ...string) sensitiveWords {
+
+	filePaths := []string{
+		filePathAd, filePathGunsExplosives, filePathPolitic, filePathWebsite,
+	}
+
+	sw := make(map[string]struct{}, 0)
+	for _, words := range skipWords {
+		sw[words] = struct{}{}
+	}
+
+	return sensitiveWords{
+		filePaths: filePaths,
+		skipWords: sw,
+	}
+}
+
+func (s *sensitiveWords) read() error {
+
+	for _, filePath := range s.filePaths {
+		sw, err := s.readFileByLines(filePath)
+		if err != nil {
+			return err
+		}
+
+		s.words = append(s.words, sw...)
+	}
+
+	return nil
+}
+
+func (s *sensitiveWords) readFileByLines(filePath string) ([]string, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open file %s: %w", filePath, err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	var sensitiveWords []string
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if _, ok := s.skipWords[line]; !ok {
+			continue
+		}
+
+		sensitiveWords = append(sensitiveWords, line)
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, fmt.Errorf("error reading file %s: %w", filePath, err)
+	}
+
+	return sensitiveWords, nil
+}
